1381b1cc9d56d8aa5f8dea2b1aeeb38de0614832
joydesigner <jason.zhengxin@gmail.com>
2015-06-18 16:57:13 +1000

    the data source updated, the value of the terms now are ranged from 5000 to 10000

--- wc.js
+++ wc.js
@@ -1,6 +1,5 @@
-﻿var keywords = ["Shanelle Kulas", "Alfredo Boehm", "Nathan Lowe III", "London Prosacco", "Miss Cecelia Rodriguez", "Ronny Anderson", "Devan Jaskolski", "Devan Jaskolski", "Freddy Kreiger", "Aric Heathcote"];
-
-var randomRotate = d3.scale.linear().domain([0, 1]).range([-20, 20]);
+﻿var keywords = ["Shanelle Kulas", "Alfredo Boehm", "Nathan Lowe III", "London Prosacco", "Miss Cecelia Rodriguez", "Ronny Anderson", "Devan Jaskolski", "Devan Jaskolski", "Freddy Kreiger", "Aric Heathcote", "Bradley Sanford Sr."];
+//var randomRotate = d3.scale.linear().domain([0, 1]).range([-20, 20]);
 
 var h = window.innerHeight-100,
     w = window.innerWidth,
@@ -14,25 +13,25 @@ var drawCloud = function (jsonFileNo) {
         var max = d3.max(data, function (d) { return d.frequency; });
 
         var dataLength = data.length;
-        var rangeMax = w / (dataLength*1.1);
+        var rangeMax = w / (dataLength);
         
         var wordScale = d3.scale.linear().domain([min, max]).range([6, rangeMax < 14 ? 14: rangeMax]).nice().clamp(true);
 
         var opacityScale = d3.scale.linear().domain([min, max]).range([0, 1]);
-        var tooltip = d3.select('.text-size')
-                                 .style('position', 'relative')
-                                 .style('padding', '0 10px')
-                                 .style('background', 'white')
-                                 .style('opacity', 0);
+       
 
         // Draw the word cloud
         var draw = function (words) {
             //zoom function
+           
+            var zoom = d3.behavior.zoom()
+                        .scaleExtent([1, 10])
+                        .on("zoom", function () {
+                            d3.select("#wordCloudG")
+                            .attr("transform", "translate(" + [midX, midY] + ")scale(" + d3.event.scale + ")");
+                        });
+         
 
-            var zoom = d3.behavior.zoom().scaleExtent([1, 10])
-                                      .on("zoom", function () {
-                                          d3.select("#wordCloudG").attr("transform", "translate(" + [midX, midY] + ")scale(" + d3.event.scale + ")");
-                                      });
             var svg = d3.select(".col-lg-12").append("svg")
                         .attr("id", "wc")
                         .attr("width", w)
@@ -40,9 +39,12 @@ var drawCloud = function (jsonFileNo) {
                         .append("g")
                         .attr("id", "wordCloudG")
                         .attr("transform", "translate(" + [midX, midY] + ")")
-                        .call(zoom);
+                        .call(zoom)
+                        .append("g")
+                        .attr("id", "#textContainer");
+                        
 
-            var cloud = svg.selectAll("g text")
+            var cloud = svg.selectAll("#textContainer text")
                         .data(words, function (d) { return d.text; });
 
             var drag = d3.behavior.drag()
@@ -64,27 +66,33 @@ var drawCloud = function (jsonFileNo) {
             cloud.enter()
                 .append("text")
                 .style("font-family", "Impact")
-                .style("fill", function (d) { return (keywords.indexOf(d.text) > -1 ? "#00CCFF" : "black"); })
+                .style("fill", function (d) { return ((keywords.indexOf(d.text) > -1) ? "#ff0000" : "black"); })
                 .attr("text-anchor", "middle")
-                .style('font-size', function (d) { return wordScale(d.frequency) + 'px'; })
+                .style('font-size', function (d) {
+                    return  (wordScale(d.frequency) + 'px');
+                })
                 .style("opacity", function (d) { return opacityScale(d.frequency); })
                 .text(function (d) { return d.text; })
                 .call(drag)
-                .on('mouseover', function (d) {
+                .on('mouseover', function (d,i) {
                     tempColor = this.style.fill;
-                    tooltip.transition().style('opacity', .9);
-                    tooltip.html("The name <span>[" + d.text + "]</span> appeared:  <span>[" + d.frequency + "] </span> times.");
-                    d3.select(this)
-                        .transition()
-                        .duration(200)
-                        .attr('cursor', 'pointer');
+                    //tooltip.transition().style('opacity', .9);
+                    var selectThis = d3.select(this);
+
+                    selectThis
+                        .append('title')
+                        .html(" <span>" + d.frequency + "</span> instances <span><br />" + d.frequency + " </span>documents");
+
+                    selectThis
+                        .attr('cursor', 'pointer')
+                        .attr('text-decoration', 'underline');
+                        
                 })
-                //.on('mouseout', function (d) {
-                //    d3.select(this)
-                //        .transition()
-                //        .duration(200)
-                //        .style('fill', tempColor);
-                //})
+                .on('mouseout', function (d) {
+                    d3.select(this)
+                        .style('fill', tempColor)
+                        .attr('text-decoration', 'none');
+                })
                 .on('click', function (d) {
                     d3.select(this)
                         .attr("stroke", "red");
@@ -100,7 +108,7 @@ var drawCloud = function (jsonFileNo) {
                 //Entering and existing words
                 cloud.transition()
                     .duration(600)
-                    .style("font-size", function (d) { return wordScale(d.frequency) + "px"; })
+                    .style("font-size", function (d) { return  wordScale(d.frequency) + "px"; })
                     .attr("transform", function (d) {
                         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                     })
@@ -117,12 +125,17 @@ var drawCloud = function (jsonFileNo) {
 
         var wordCloud = d3.layout.cloud().size([w, h])
             .timeInterval(10)
+            .spiral("archimedean")
+            //.spiral('rectangular')
             .words(data)
-            .padding(8, 10)
+            .padding(1)
+            
             //.rotate(function (d) { return d.text.length > 12 ? 0 : 90; })
             .rotate(0)
-            .font("Impact")
+            //.font("Impact")
+            .text(function (d) { return d.text;})
             .fontSize(function (d) { return wordScale(d.frequency); })
+            
             .on("end", draw)
             .start();
     });
